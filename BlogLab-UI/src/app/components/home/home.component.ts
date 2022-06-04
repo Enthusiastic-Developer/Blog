a0ff5778131359bc28707e3e721ec28bb5654ae1
Nikhil <nikhilbanuri4@gmail.com>
2022-06-04 16:39:00 +0530

    home component twik is done

--- BlogLab-UI/src/app/components/home/home.component.ts
+++ BlogLab-UI/src/app/components/home/home.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from 'src/app/services/account.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  status!: boolean;
+  constructor(public accountService1: AccountService) {}
+  getstatus() {
+    this.status = this.accountService1.isLoggedIn();
   }
 
+  ngOnInit(): void {
+    this.getstatus();
+  }
 }
